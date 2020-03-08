d9abe4b6a5d7c9045d82c6df38587ea1314702d0
a-m-dev <a.mirzaei.dev@gmail.com>
2020-03-08 22:55:59 +0330

    code splliting done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 
-import SearchParams from "./SearchParams";
-import Details from "./Details";
-
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const themeHook = useState("darkblue");
@@ -17,10 +17,13 @@ const App = () => {
           <header>
             <Link to="/">Adopt Me!</Link>
           </header>
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+
+          <Suspense fallback={<h1>Loading Route</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
